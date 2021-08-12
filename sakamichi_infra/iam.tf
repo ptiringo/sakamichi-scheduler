8e96618f186f195a03ec1860737a29afc3227d99
Matsui <ptiringo@gmail.com>
2021-08-12 20:09:19 +0900

    use new dedicated service account for cloud run service

--- sakamichi_infra/iam.tf
+++ sakamichi_infra/iam.tf
@@ -1,3 +1,6 @@
+#----------------------------------------------------------------------
+# google_project_iam_member
+#----------------------------------------------------------------------
 resource "google_project_iam_member" "ci_executor_artifactregistry_writer" {
   role   = "roles/artifactregistry.writer"
   member = "serviceAccount:${google_service_account.ci_executor.email}"
@@ -11,4 +14,13 @@ resource "google_project_iam_member" "ci_executor_run_developer" {
 resource "google_project_iam_member" "ci_executor_service_account_user" {
   role   = "roles/iam.serviceAccountUser"
   member = "serviceAccount:${google_service_account.ci_executor.email}"
+}
+
+#----------------------------------------------------------------------
+# google_service_account_iam_member
+#----------------------------------------------------------------------
+resource "google_service_account_iam_member" "sakamichi_scraper_executor_service_account_user_ci_executor" {
+  service_account_id = google_service_account.sakamichi_scraper_executor.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${google_service_account.ci_executor.email}"
 }
--- sakamichi_infra/service_account.tf
+++ sakamichi_infra/service_account.tf
@@ -2,11 +2,22 @@ data "google_compute_default_service_account" "compute_default" {
   depends_on = [google_project_service.compute]
 }
 
+#----------------------------------------------------------------------
+# google_service_account
+#----------------------------------------------------------------------
 resource "google_service_account" "ci_executor" {
   account_id   = "ci-executor"
   display_name = "CI Executor"
 }
 
+resource "google_service_account" "sakamichi_scraper_executor" {
+  account_id   = "sakamichi-scraper-executor"
+  display_name = "sakamichi-scraper Executor"
+}
+
+#----------------------------------------------------------------------
+# google_service_account_key
+#----------------------------------------------------------------------
 resource "google_service_account_key" "ci_executor" {
   service_account_id = google_service_account.ci_executor.name
   depends_on         = [google_project_service.compute]
