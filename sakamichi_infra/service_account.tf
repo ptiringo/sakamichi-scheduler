722cfee984832750ac3485d387d4b97e354b0190
Matsui <ptiringo@gmail.com>
2021-08-12 21:34:08 +0900

    add invoker

--- sakamichi_infra/service_account.tf
+++ sakamichi_infra/service_account.tf
@@ -1,3 +1,6 @@
+#----------------------------------------------------------------------
+# google_compute_default_service_account
+#----------------------------------------------------------------------
 data "google_compute_default_service_account" "compute_default" {
   depends_on = [google_project_service.compute]
 }
@@ -13,6 +16,11 @@ resource "google_service_account" "ci_executor" {
 resource "google_service_account" "sakamichi_scraper_executor" {
   account_id   = "sakamichi-scraper-executor"
   display_name = "sakamichi-scraper Executor"
+}
+
+resource "google_service_account" "sakamichi_scraper_invoker" {
+  account_id   = "sakamichi-scraper-invoker"
+  display_name = "sakamichi-scraper Invoker"
 }
 
 #----------------------------------------------------------------------
