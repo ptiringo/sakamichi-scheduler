5d6240a2c3ebdf66f4b44ee3f9d8dac6c011ab87
Matsui <ptiringo@gmail.com>
2021-08-19 22:07:52 +0900

    update

--- sakamichi_noticer/deploy.sh
+++
@@ -1,7 +0,0 @@
-gcloud functions deploy update-schedule-hinata \
-  --entry-point tokyo.sakamichi_noticer.UpdateSchedule \
-  --runtime java11 \
-  --memory 512MB \
-  --trigger-resource hinata-schedule \
-  --trigger-event google.storage.object.finalize \
-  --project sakamichi-noticer
