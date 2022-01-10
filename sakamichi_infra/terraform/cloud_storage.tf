268b67d640405109deec1b43e41f30e16710cdcc
Matsui <ptiringo@gmail.com>
2022-01-10 22:27:59 +0900

    add bucket 'nogi_schedule'

--- sakamichi_infra/terraform/cloud_storage.tf
+++ sakamichi_infra/terraform/cloud_storage.tf
@@ -3,3 +3,9 @@ resource "google_storage_bucket" "hinata_schedule" {
   location      = "ASIA-NORTHEAST1"
   storage_class = "STANDARD"
 }
+
+resource "google_storage_bucket" "nogi_schedule" {
+  name          = "nogi-schedule"
+  location      = "ASIA-NORTHEAST1"
+  storage_class = "STANDARD"
+}
