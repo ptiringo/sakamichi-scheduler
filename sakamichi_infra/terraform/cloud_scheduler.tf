a11d478250b681fe4514fe469e63175d42ef5944
Matsui <ptiringo@gmail.com>
2022-01-11 22:58:48 +0900

    add google_cloud_scheduler_job.collect_nogi_schedule

--- sakamichi_infra/terraform/cloud_scheduler.tf
+++ sakamichi_infra/terraform/cloud_scheduler.tf
@@ -12,3 +12,18 @@ resource "google_cloud_scheduler_job" "collect_hinata_schedule" {
     }
   }
 }
+
+resource "google_cloud_scheduler_job" "collect_nogi_schedule" {
+  name      = "collect-nogi-schedule"
+  schedule  = "every 6 hours"
+  time_zone = "Asia/Tokyo"
+
+  http_target {
+    http_method = "GET"
+    uri         = "${data.google_cloud_run_service.sakamichi_scraper.status[0].url}/crawl.json?spider_name=nogi_schedule&start_requests=true"
+
+    oidc_token {
+      service_account_email = google_service_account.sakamichi_scraper_invoker.email
+    }
+  }
+}
