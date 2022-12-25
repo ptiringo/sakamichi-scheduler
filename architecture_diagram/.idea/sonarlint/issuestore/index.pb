cf1422332e4307d96f23dc81c49c7d6f9703e6f6
Matsui <ptiringo@gmail.com>
2022-12-25 22:13:39 +0900

    remove deployment manager

---
+++ architecture_diagram/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,6 @@
+
+:
+
+diagram.py,9/b/9b7e88d3a54e59f86fd43ec1ba310fb80ccc5eb2
+>
+pyproject.toml,5/d/5d07e7d72637aa0d59c89d381fe6dc4cf46e2491
---
+++ sakamichi_infra/terraform/monitoring.tf
@@ -0,0 +1,29 @@
+resource "google_monitoring_notification_channel" "mail" {
+  display_name = "Mail Notification"
+  type         = "email"
+  labels = {
+    email_address = "ptiringo@gmail.com"
+  }
+}
+
+resource "google_monitoring_alert_policy" "error_log" {
+  display_name          = "Error Log"
+  combiner              = "OR"
+  notification_channels = [google_monitoring_notification_channel.mail.name]
+
+  alert_strategy {
+    notification_rate_limit {
+      period = "300s"
+    }
+  }
+
+  conditions {
+    display_name = "Cloud Function Error"
+
+    condition_matched_log {
+      filter = <<-EOT
+      resource.type=("cloud_function" OR "cloud_run_revision" OR "cloud_scheduler_job") severity>=ERROR
+      EOT
+    }
+  }
+}
