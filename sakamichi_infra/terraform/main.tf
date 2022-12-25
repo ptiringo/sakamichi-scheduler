44a38d4f1baec9ae23c54d8643d80abb9b59c0bf
Matsui <ptiringo@gmail.com>
2022-12-25 22:23:58 +0900

    upgrade terraform version

--- sakamichi_infra/terraform/main.tf
+++ sakamichi_infra/terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "=1.3.6"
+  required_version = "~> 1.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.45.0"
+      version = "~> 4.0"
     }
   }
 
